62528f20f7125040248264ff23988ae69799d322
ashueep <ashuanand29@gmail.com>
2021-02-28 18:07:51 +0400

    13

--- 13/riscv4.asm
+++ 13/riscv4.asm
@@ -19,28 +19,30 @@
 	li a0,10
 	sw a0,0(t1)
 	
-	li a1,1
+	li a1,6
 	
-	la t1,Array
-	addi s0,t1,16
+	li t1,0
+	addi s0,t1,3
 	la t4,Array
 	
 bsearch:
-	bge t1,s0,not_found
+	bgt t1,s0,not_found
 	sub s1,s0,t1
 	srli s1,s1,1
-	add t2,t4,s1
-	lw t3,(t2)
+	add s1,s1,t1
+	slli s2,s1,2
+	add s2,t4,s2
+	lw t3,(s2)
 	beq t3,a1,found
 	blt t3,a1,c2
 	bgt t3,a1,c3
 	
 c2:
-	addi t1,t2,4
+	addi t1,s1,1
 	j bsearch
 
 c3:
-	addi s0,t2,-4
+	addi s0,s1,-1
 	j bsearch
 	
 found:
@@ -48,3 +50,4 @@ found:
 
 not_found:
 	li s9,1
+
