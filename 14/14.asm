60c9fa9aeef235a465858a703ba8a7f5d828369a
ashueep <ashuanand29@gmail.com>
2021-02-28 21:51:29 +0400

    14

--- 14/14.asm
+++ 14/14.asm
@@ -1,6 +1,6 @@
 .data 
-	tell: .string "The postfix of the infix string 'a+b-c*d/e+f' is: "
-	infix: .string "a+b-c*d/e+f"
+	tell: .string "The postfix of the infix string 'a+b-c*d/(e+f)' is: "
+	infix: .string "a+b-c*d/(e+f)"
 .text
 	la a0,tell
 	li a7,4
@@ -8,7 +8,7 @@
 	addi s6,sp,0
 	la t1, infix
 	li t4,0
-	li t3,12
+	li t3,14
 	loop:
 		bge t4,t3,exit_loop
 		add t5,t1,t4
@@ -22,9 +22,9 @@
 		li t2,'/'
 		beq s0,t2,operator
 		li t2,'('
-		beq s0,t2,operator
+		beq s0,t2,push
 		li t2,')'
-		beq s0,t2,operator
+		beq s0,t2,pop_till_open
 		j operand
 
 		continue:
@@ -59,6 +59,22 @@ popping:
 	ecall
 	addi sp,sp,2
 	j operator
+	
+pop_till_open:
+	li s10,'('
+	lb s11,2(sp)
+	beq s11,s10,pop_open
+	addi a0,s11,0
+	li a7,11
+	ecall
+	addi sp,sp,2
+	addi s1,s1,-1
+	j pop_till_open
+	
+pop_open:
+	addi s1,s1,-1
+	addi sp,sp,2
+	j continue
 
 precedence_of_s0:
 	li t2,'+'
@@ -91,6 +107,12 @@ precedence_of_stack:
 	beq s5,t2,mul_div_stack
 	li t2,'/'
 	beq s5,t2,mul_div_stack
+	li t2,'('
+	beq s5,t2,prec_of_open
+
+prec_of_open:
+	li s4,-1
+	j continue_prec
 
 plus_minus_stack:
 	li s4,1
